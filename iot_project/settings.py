bcf394dc8533edaf488dee0a46e6b3b49724826f
anib9871 <anib9871@gmail.com>
2025-09-26 17:51:51 +0530

    Update settings.py

--- iot_project/settings.py
+++ iot_project/settings.py
@@ -74,12 +74,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'iot_project.wsgi.application'
 
 # SQLite
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
+
 DATABASES = {
     "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
