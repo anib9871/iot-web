5f3a8b77dc6f3fe4e5e4cbf09d089d6fa3c9285b
anib9871 <anib9871@gmail.com>
2025-09-26 18:21:44 +0530

    Update 0006_alter_devicereadinglog_reading_time.py

--- iot_api/migrations/0006_alter_devicereadinglog_reading_time.py
+++ iot_api/migrations/0006_alter_devicereadinglog_reading_time.py
@@ -1,3 +1,5 @@
+# iot_api/migrations/0006_alter_devicereadinglog_reading_time.py
+
 from django.db import migrations, models
 
 class Migration(migrations.Migration):
@@ -7,16 +9,22 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        # Step 1: Django ko field type ka update batao
         migrations.AlterField(
             model_name='devicereadinglog',
             name='READING_TIME',
             field=models.TimeField(),
         ),
+        # Step 2: PostgreSQL me integer -> time conversion
         migrations.RunSQL(
             """
             ALTER TABLE iot_api_devicereadinglog
             ALTER COLUMN "READING_TIME" TYPE time
-            USING make_time("READING_TIME"/10000, ("READING_TIME"/100)%100, "READING_TIME"%100);
+            USING make_time(
+                "READING_TIME" / 10000,
+                ("READING_TIME" / 100) % 100,
+                "READING_TIME" % 100
+            );
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),
